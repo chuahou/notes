b1ce2b9be0dc8d03fde28777d5ecb94caec1b9f5
Chua Hou <human+github@chuahou.dev>
2020-11-15 01:19:59 +0800

    build.sh: remove biolinum-font package and only build outdated files

--- build.sh
+++ build.sh
@@ -2,4 +2,19 @@
 # SPDX-License-Identifier: MIT
 # Copyright (c) 2020 Chua Hou
 
-texmacs $(find . -type f -name '*.tm' | sed 's/\(.*\)\.tm/-c \1.tm \1.html/') -q
+# find all .tm files newer than their .html counterparts
+for tmfile in $(find . -type f -name '*.tm'); do
+	if [ $tmfile -nt $(sed 's/\.tm/\.html/' <<< $tmfile) ]; then
+		# remove biolinum-font package from .tm file, saving results in
+		# temporary file
+		sed 's/biolinum-font|//' $tmfile > $tmfile.tmp
+		sed -i 's/biolinum-font>/>/' $tmfile.tmp
+	fi
+done
+
+# pass all created temporary files to convert to texmacs
+texmacs $(find . -type f -name '*.tm.tmp' |
+	sed 's/\(.*\)\.tm.tmp/-c \1.tm.tmp \1.html/') -q
+
+# remove created temporary files
+find . -type f -name '*.tm.tmp' -exec rm {} \;

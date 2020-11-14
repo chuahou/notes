ba9ec5f79b5305cb19ab046e87d07d3fc77a92d7
Chua Hou <human+github@chuahou.dev>
2020-11-15 01:20:17 +0800

    notesStyle.ts: add

---
+++ notesStyle.ts
@@ -0,0 +1,50 @@
+<TeXmacs|1.99.15>
+
+<style|source>
+
+<\body>
+  <\active*>
+    <\src-title>
+      <src-package|notesStyle|1.0>
+
+      <src-purpose|Style package for notes.>
+    </src-title>
+  </active*>
+
+  <\active*>
+    <\src-comment>
+      Add link to root CSS style.
+    </src-comment>
+  </active*>
+
+  <provide|html-extra-css|<tuple|/style.css>>
+
+  <\active*>
+    <\src-comment>
+      Style parameters.
+    </src-comment>
+  </active*>
+
+  <assign|full-screen-mode|false>
+
+  <assign|page-medium|paper>
+
+  <assign|project-flag|true>
+
+  <\active*>
+    <\src-comment>
+      Macro definitions.
+    </src-comment>
+  </active*>
+
+  <assign|verbatim|<macro|body|<small|<with|font|Ioslabka|language|verbatim|<arg|body>>>>>
+</body>
+
+<\initial>
+  <\collection>
+    <associate|font|roman>
+    <associate|font-family|rm>
+    <associate|math-font|roman>
+    <associate|preamble|true>
+  </collection>
+</initial>
